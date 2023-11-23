6c07fa278f0e8e45de9bb5dd1ab017300f8521df
ExostFlash <120869320+ExostFlash@users.noreply.github.com>
2023-11-23 11:35:50 +0100

    Block acces to admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -80,6 +80,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
+        $grade = $this->grade;
+
+        $roles[] = "ROLE_" . (string)$grade;
 
         return array_unique($roles);
     }
